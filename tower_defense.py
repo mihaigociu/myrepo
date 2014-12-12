837471307591e10c0e717c5e1f6caab54f812eaf
Mihai Gociu <mihai.gociu@spyhce.com>
2014-12-12 14:47:41 +0200

    Update tower_defense.py

--- tower_defense.py
+++ tower_defense.py
@@ -215,7 +215,7 @@ class Action(object):
         self.attrs = attrs
 
     def __str__(self):
-        return '%s - %s' % (self.action_type, self.frame)
+        return '%s - %s - %s' % (self.action_type, self.attrs, self.frame)
 
     def __repr__(self):
         return self.__str__()
